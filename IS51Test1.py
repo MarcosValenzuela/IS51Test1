824f40655f1b93c31f732539f6e0353ce46c9158
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-11-12 16:31:26 -0800

    structured-english finished

---
+++ IS51Test1.py
@@ -0,0 +1,15 @@
+"""
+The program is going to decide which payment option is better
+The first option is $100 dollars a day for ten days.
+The second option is for 10 days starting at $1 per day it will double.
+Two functions are need to find how much is made for each option.
+Function1 will calculate the amount for option 1.
+Function2 will calculate the amounbt for option 2.
+
+function1 will ouput $100 * 10 days.
+function2 will loop for 10 days, each day the pay rate will double and add that amount to total
+
+If options are equal, we output "Each option makes the same amount" show amount
+If option1 makes more, we output "Option1 makes more at: $"
+If option2 makes more, we output "Option2 makes more at: $"
+"""
